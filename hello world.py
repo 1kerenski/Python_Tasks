4e5cabdf873ada0de0398aaabf5cefca0bb5fcb9
Vlad Kerenski <140814161+1kerenski@users.noreply.github.com>
2023-07-28 20:56:42 +0300

    39. Сколько дней в месяце?

---
+++ hello world.py
@@ -0,0 +1,25 @@
+x = input('Введите месяц: ')
+if x == 'Январь':
+    print('31')
+elif x == 'Февраль':
+    print('28 или 29')
+elif x == 'Март':
+    print('31')
+elif x == 'Апрель':
+    print('30')
+elif x == 'Май':
+    print('31')
+elif x == 'Июнь':
+    print('30')
+elif x == 'Июль':
+    print('31')
+elif x == 'Август':
+    print('31')
+elif x == 'Сентябрь':
+    print('30')
+elif x == 'Октябрь':
+    print('31')
+elif x == 'Ноябрь':
+    print('30')
+elif x == 'Декабрь':
+    print('31')
